661dd6d56fe83b18e57e73b80d45553574979e21
lapinell <lapinell@gmail.com>
2020-03-09 18:01:15 -0500

    added purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,15 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './*.html',
+  ],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
   map: true,
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'development' ? [purgecss] : []
   ]
 }
