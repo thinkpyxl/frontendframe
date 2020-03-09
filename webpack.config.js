b5e778fd67ab269628f59bf7fe2e0d8442338ec7
lapinell <lapinell@gmail.com>
2020-03-09 14:38:59 -0500

    testing postcss loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,14 +13,16 @@ module.exports = {
   },
   
   module: {
-    rules: [{
+    rules: [
+      {
       test: /\.css$/,
       use: [
         'style-loader',
         { loader: 'css-loader', options: { importLoaders: 1 } },
         'postcss-loader'
-      ]
-    }]
+        ]
+      }
+    ]
   }
 
 }
