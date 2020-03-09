46b6a47310f08c2a4ed9ba61b758399411263c18
lapinell <lapinell@gmail.com>
2020-03-09 13:36:05 -0500

    added postcss to configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,23 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
+
   entry: './src/index.js',
+  
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
+  
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        { loader: 'css-loader', options: { importLoaders: 1 } },
+        'postcss-loader'
+      ]
+    }]
+  }
+
 }
