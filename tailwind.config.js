b6d96e52ffe873f6356e4db29e73328905e752cb
Laura Pinell <lapinell@gmail.com>
2020-03-27 16:48:29 -0500

    updated config and package.json, and tested

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,6 @@ module.exports = {
       md: '768px',
       lg: '1440px',
       xl: '1920px'
-    },
-    fontFamily: {
-      display: ['Bellota Text', 'cursive'],
-      body: ['Roboto', 'sans-setif'],
-    },
-    colors: {
-      grey: '#48565D',
-      teal: '#84A39E',
-      black: '#232B32',
-      tan: '#958E74',
-      white: '#F7F9F9'
     }
   }
 }
