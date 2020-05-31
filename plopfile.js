421a334715fac4827070870f6bd16165786c8fb1
LuosRestil <metaltasteslikechicken@gmail.com>
2020-05-31 14:36:33 -0500

    Updated Plopfile and .css generator per team spec

--- plopfile.js
+++ plopfile.js
@@ -1,77 +1,181 @@
 module.exports = function (plop) {
-    plop.setGenerator('atom creation', {
-        description: 'atom creation',
+    plop.setGenerator('archive creation', {
+        description: 'archive creation',
         prompts: [{
             type: 'input',
             name: 'name',
-            message: 'atom name please'
+            message: 'Enter archive name'
         }],
         actions: [
             {
                 type: 'add',
-                path: 'src/atoms/{{lowerCase name}}/{{lowerCase name}}.css',
+                path: 'src/archive/{{lowerCase name}}/{{lowerCase name}}.css',
                 templateFile: 'generators/basic/basic.css',
             },
             {
                 type: 'add',
-                path: 'src/atoms/{{lowerCase name}}/{{lowerCase name}}.js',
+                path: 'src/archive/{{lowerCase name}}/{{lowerCase name}}.js',
                 templateFile: 'generators/basic/basic.js',
             },
             {
                 type: 'add',
-                path: 'src/atoms/{{lowerCase name}}/{{lowerCase name}}.twig',
+                path: 'src/archive/{{lowerCase name}}/{{lowerCase name}}.twig',
                 templateFile: 'generators/basic/basic.twig',
             },
         ]
     });
 
-    plop.setGenerator('molecule creation', {
-        description: 'molecule creation',
+    plop.setGenerator('block creation', {
+        description: 'block creation',
         prompts: [{
             type: 'input',
             name: 'name',
-            message: 'molecule name please'
+            message: 'Enter block name'
         }],
         actions: [
             {
                 type: 'add',
-                path: 'src/molecules/{{lowerCase name}}/{{lowerCase name}}.css',
+                path: 'src/block/{{lowerCase name}}/{{lowerCase name}}.css',
                 templateFile: 'generators/basic/basic.css',
             },
             {
                 type: 'add',
-                path: 'src/molecules/{{lowerCase name}}/{{lowerCase name}}.js',
+                path: 'src/block/{{lowerCase name}}/{{lowerCase name}}.js',
                 templateFile: 'generators/basic/basic.js',
             },
             {
                 type: 'add',
-                path: 'src/molecules/{{lowerCase name}}/{{lowerCase name}}.twig',
+                path: 'src/block/{{lowerCase name}}/{{lowerCase name}}.twig',
                 templateFile: 'generators/basic/basic.twig',
             },
         ]
     });
 
-    plop.setGenerator('organism creation', {
-        description: 'organism creation',
+    plop.setGenerator('component creation', {
+        description: 'component creation',
         prompts: [{
             type: 'input',
             name: 'name',
-            message: 'organism name please'
+            message: 'Enter component name'
         }],
         actions: [
             {
                 type: 'add',
-                path: 'src/organisms/{{lowerCase name}}/{{lowerCase name}}.css',
+                path: 'src/component/{{lowerCase name}}/{{lowerCase name}}.css',
                 templateFile: 'generators/basic/basic.css',
             },
             {
                 type: 'add',
-                path: 'src/organisms/{{lowerCase name}}/{{lowerCase name}}.js',
+                path: 'src/component/{{lowerCase name}}/{{lowerCase name}}.js',
                 templateFile: 'generators/basic/basic.js',
             },
             {
                 type: 'add',
-                path: 'src/organisms/{{lowerCase name}}/{{lowerCase name}}.twig',
+                path: 'src/component/{{lowerCase name}}/{{lowerCase name}}.twig',
+                templateFile: 'generators/basic/basic.twig',
+            },
+        ]
+    });
+
+    plop.setGenerator('four-oh-four creation', {
+        description: 'four-oh-four creation',
+        prompts: [{
+            type: 'input',
+            name: 'name',
+            message: 'Enter four-oh-four name'
+        }],
+        actions: [
+            {
+                type: 'add',
+                path: 'src/four-oh-four/{{lowerCase name}}/{{lowerCase name}}.css',
+                templateFile: 'generators/basic/basic.css',
+            },
+            {
+                type: 'add',
+                path: 'src/four-oh-four/{{lowerCase name}}/{{lowerCase name}}.js',
+                templateFile: 'generators/basic/basic.js',
+            },
+            {
+                type: 'add',
+                path: 'src/four-oh-four/{{lowerCase name}}/{{lowerCase name}}.twig',
+                templateFile: 'generators/basic/basic.twig',
+            },
+        ]
+    });
+
+    plop.setGenerator('global creation', {
+        description: 'global creation',
+        prompts: [{
+            type: 'input',
+            name: 'name',
+            message: 'Enter global name'
+        }],
+        actions: [
+            {
+                type: 'add',
+                path: 'src/global/{{lowerCase name}}/{{lowerCase name}}.css',
+                templateFile: 'generators/basic/basic.css',
+            },
+            {
+                type: 'add',
+                path: 'src/global/{{lowerCase name}}/{{lowerCase name}}.js',
+                templateFile: 'generators/basic/basic.js',
+            },
+            {
+                type: 'add',
+                path: 'src/global/{{lowerCase name}}/{{lowerCase name}}.twig',
+                templateFile: 'generators/basic/basic.twig',
+            },
+        ]
+    });
+
+    plop.setGenerator('search creation', {
+        description: 'search creation',
+        prompts: [{
+            type: 'input',
+            name: 'name',
+            message: 'Enter search name'
+        }],
+        actions: [
+            {
+                type: 'add',
+                path: 'src/search/{{lowerCase name}}/{{lowerCase name}}.css',
+                templateFile: 'generators/basic/basic.css',
+            },
+            {
+                type: 'add',
+                path: 'src/search/{{lowerCase name}}/{{lowerCase name}}.js',
+                templateFile: 'generators/basic/basic.js',
+            },
+            {
+                type: 'add',
+                path: 'src/search/{{lowerCase name}}/{{lowerCase name}}.twig',
+                templateFile: 'generators/basic/basic.twig',
+            },
+        ]
+    });
+
+    plop.setGenerator('single creation', {
+        description: 'single creation',
+        prompts: [{
+            type: 'input',
+            name: 'name',
+            message: 'Enter single name'
+        }],
+        actions: [
+            {
+                type: 'add',
+                path: 'src/single/{{lowerCase name}}/{{lowerCase name}}.css',
+                templateFile: 'generators/basic/basic.css',
+            },
+            {
+                type: 'add',
+                path: 'src/single/{{lowerCase name}}/{{lowerCase name}}.js',
+                templateFile: 'generators/basic/basic.js',
+            },
+            {
+                type: 'add',
+                path: 'src/single/{{lowerCase name}}/{{lowerCase name}}.twig',
                 templateFile: 'generators/basic/basic.twig',
             },
         ]
